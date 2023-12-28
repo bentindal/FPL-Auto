2258df87406ab84e6420db94d86ff7d38b2f433a
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-12-28 17:45:33 +0000

    Model retrained each gameweek

--- main.py
+++ main.py
@@ -15,79 +15,117 @@ from sklearn import linear_model, tree
 import matplotlib.pyplot as plt
 
 target_season = '2023-24'
-target_gameweek = 14
+target_gameweek = 9
+last_gameweek = 18
 fplapi = fplapi_data(target_season)
 vastaav = vastaav_data('../Fantasy-Premier-League/data', target_season)
 eval = fpl_evaluate()
 
 def main():
-    print('Extracting data & reducing feature set...')
-    feature_names, week_data = vastaav.get_training_data(target_gameweek - 1)
+    # Predict points for GWi
+    for i in range(target_gameweek, target_gameweek + 10):
+        # Retrain model each time
+        # Lets sum up the last 3 gameweeks to get a more accurate representation of player performance
+        if i >= last_gameweek:
+            i = last_gameweek - 1
+            
+        __, feature_names, week_data = vastaav.get_training_data(i - 1)
 
-    # Lets train a model
-    print('Training model...')
-    gk_model = linear_model.LinearRegression()
-    gk_model.fit(week_data[0][0], week_data[0][1])
+        # Lets train a model
+        gk_model = linear_model.LinearRegression()
+        gk_model.fit(week_data[0][0], week_data[0][1])
 
-    def_model = linear_model.LinearRegression()
-    def_model.fit(week_data[1][0], week_data[1][1])
+        def_model = linear_model.LinearRegression()
+        def_model.fit(week_data[1][0], week_data[1][1])
 
-    mid_model = linear_model.LinearRegression()
-    mid_model.fit(week_data[2][0], week_data[2][1])
+        mid_model = linear_model.LinearRegression()
+        mid_model.fit(week_data[2][0], week_data[2][1])
 
-    fwd_model = linear_model.LinearRegression()
-    fwd_model.fit(week_data[3][0], week_data[3][1])
+        fwd_model = linear_model.LinearRegression()
+        fwd_model.fit(week_data[3][0], week_data[3][1])
 
-    print('Models trained!')
-    
-    # Plot the weights to each feature
-    gk_weights = gk_model.coef_
-    def_weights = def_model.coef_
-    mid_weights = mid_model.coef_
-    fwd_weights = fwd_model.coef_
+        # Plot the weights to each feature
+        weights = np.concatenate((gk_model.coef_, def_model.coef_, mid_model.coef_, fwd_model.coef_))
+        feature_names = np.concatenate((feature_names[0], feature_names[1], feature_names[2], feature_names[3]))
 
-    weights = np.concatenate((gk_weights, def_weights, mid_weights, fwd_weights))
-    feature_names = np.concatenate((feature_names[0], feature_names[1], feature_names[2], feature_names[3]))
+        plt.title(f'Feature weights for GW{i}')
+        plt.xlabel('Feature')
+        plt.ylabel('Weight')
+        plt.bar(feature_names, weights)
+        plt.xticks(rotation=90)
+        plt.show()
 
-    plt.title('Feature weights')
-    plt.xlabel('Feature')
-    plt.ylabel('Weight')
-    plt.bar(feature_names, weights)
-    plt.xticks(rotation=90)
-    plt.show()
-    
 
-    for i in range(target_gameweek, target_gameweek + 3):
-        feature_names, week_data = vastaav.get_training_data(i)
+        # target_gameweek has not happened yet, so we predict it using past data 
+        # Get test data by summing up 3 previous gameweeks
+        # week_data = GWi-3 + GWi-2 + GWi-1
+        if i == last_gameweek - 1:
+            print(f'Predicting FUTURE GW{i}...')
+            # Get features from most up to date data
+            player_names, feature_names, features = vastaav.get_test_data(last_gameweek - 1)
+        else: 
+            player_names, feature_names, test_data = vastaav.get_training_data(i)
+        
+            # Get the features and labels from the test data
+            features = [test_data[0][0], test_data[1][0], test_data[2][0], test_data[3][0]]
+            labels = [test_data[0][1], test_data[1][1], test_data[2][1], test_data[3][1]] 
 
-        # Predict points for GWi
-        print(f'Predicting points for GW{i}...')
-        gk_predictions = gk_model.predict(week_data[0][0])
-        def_predictions = def_model.predict(week_data[1][0])
-        mid_predictions = mid_model.predict(week_data[2][0])
-        fwd_predictions = fwd_model.predict(week_data[3][0])
+        gk_predictions = gk_model.predict(features[0])
+        def_predictions = def_model.predict(features[1])
+        mid_predictions = mid_model.predict(features[2])
+        fwd_predictions = fwd_model.predict(features[3])
 
-        eval.score_model(gk_predictions, week_data[0][1])
-        eval.score_model(def_predictions, week_data[1][1])
-        eval.score_model(mid_predictions, week_data[2][1])
-        eval.score_model(fwd_predictions, week_data[3][1])
+        if i < last_gameweek - 1:
+            gk_error, gk_square_error, gk_accuracy = eval.score_model(gk_predictions, labels[0])
+            def_error, def_square_error, def_accuracy = eval.score_model(def_predictions, labels[1])
+            mid_error, mid_square_error, mid_accuracy = eval.score_model(mid_predictions, labels[2])
+            fwd_error, fwd_square_error, fwd_accuracy = eval.score_model(fwd_predictions, labels[3])
 
-        # Plot predictions vs actual points for GWi
+            # Average the errors
+            error = (gk_error + def_error + mid_error + fwd_error) / 4
+            ase = (gk_square_error + def_square_error + mid_square_error + fwd_square_error) / 4
+            aa = (gk_accuracy + def_accuracy + mid_accuracy + fwd_accuracy) / 4
+            print(f'GW{i}, AE: {error:.2f}, ASE: {ase:.2f}, ACC: {aa*100:.2f}%')
+
+        if i < last_gameweek - 1:
+            # Output predictions to csv, use player_names as first column, then predictions, then actual points
+            # This will allow us to compare predictions to actual points
+            csv_gk_predictions = np.column_stack((player_names[0], gk_predictions, labels[0]))
+            csv_def_predictions = np.column_stack((player_names[1], def_predictions, labels[1]))
+            csv_mid_predictions = np.column_stack((player_names[2], mid_predictions, labels[2]))
+            csv_fwd_predictions = np.column_stack((player_names[3], fwd_predictions, labels[3]))
+            
+            csv_gk_predictions = np.concatenate((np.array([['Name', 'Predicted points', 'Actual Points']]), csv_gk_predictions), axis=0)
+        else:
+            csv_gk_predictions = np.column_stack((player_names[0], gk_predictions))
+            csv_def_predictions = np.column_stack((player_names[1], def_predictions))
+            csv_mid_predictions = np.column_stack((player_names[2], mid_predictions))
+            csv_fwd_predictions = np.column_stack((player_names[3], fwd_predictions))
+
+            csv_gk_predictions = np.concatenate((np.array([['Name', 'Predicted points']]), csv_gk_predictions), axis=0)
+        
+        csv_predictions = np.concatenate((csv_gk_predictions, csv_def_predictions, csv_mid_predictions, csv_fwd_predictions), axis=0)
+
+        csv_predictions = pd.DataFrame(csv_predictions[1:], columns=csv_predictions[0])
+        csv_predictions.set_index('Name', inplace=True)
+
+        csv_predictions.to_csv(f'predictions/predictions_gw{i}.csv')
+        
+        '''# Plot predictions vs actual points for GWi
         # Colour code by position
         plt.title(f'GW{i} predictions vs actual points')
         plt.xlabel('Predicted points')
         plt.ylabel('Actual points')
-        plt.scatter(gk_predictions, week_data[0][1], color='red')
-        plt.scatter(def_predictions, week_data[1][1], color='blue')
-        plt.scatter(mid_predictions, week_data[2][1], color='green')
-        plt.scatter(fwd_predictions, week_data[3][1], color='orange')
+        print(len(gk_predictions))
+        print(len(labels[0]))
+        plt.scatter(gk_predictions, labels[0], color='red')
+        plt.scatter(def_predictions, labels[1], color='blue')
+        plt.scatter(mid_predictions, labels[2], color='green')
+        plt.scatter(fwd_predictions, labels[3], color='orange')
         plt.legend(['GK', 'DEF', 'MID', 'FWD'])
         # Plot y=x line to show perfect prediction
         plt.plot([0, 20], [0, 20], color='black')
-        # Plot line of best fit
-        
-
-        plt.show()
+        plt.show()'''
 
 
 
--- vastaav.py
+++ vastaav.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-import requests, json
+import requests
+import json
 from pprint import pprint
 import json
 
@@ -20,66 +21,18 @@ class vastaav_data:
     
     def get_team_list(self):
         team_list = pd.read_csv(f'{self.data_location}/teams.csv')
-        # Drop all columns except for team name and team id
         team_list = team_list[['name', 'strength_overall_home', 'strength_overall_away', 'strength_attack_home', 'strength_attack_away', 'strength_defence_home', 'strength_defence_away']]
         return team_list.set_index('name')
     
     def get_gw_data(self, week_num):
         gw_data = pd.read_csv(f'{self.data_location}/gws/gw{week_num}.csv')
-        # Drop all columns except for player name and position
         gw_data = gw_data[['name', 'position', 'team', 'assists', 'bps', 'clean_sheets', 'creativity', 'goals_conceded', 'goals_scored', 'ict_index', 'influence', 'minutes', 'own_goals', 'penalties_missed', 'penalties_saved', 'red_cards', 'saves', 'threat', 'total_points', 'yellow_cards', 'selected', 'was_home']]
         return gw_data.set_index('name')
 
     def get_pos_data(self, week_num, position):
         gw_data = self.get_gw_data(week_num)
-        # Drop all players that are not of the specified position
         gw_data = gw_data[gw_data['position'] == position]
-
-        # Goalkeepers
-        # Drop goals_scored, assists, threat
-        # Add based on team: strength_defence_home, strength_defence_away
-        if position == 'GK':
-            gw_data = gw_data.drop(['goals_scored', 'assists', 'threat'], axis=1)
-            '''for index, row in gw_data.iterrows():
-                if row['was_home']:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_defence_home']
-                else:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_defence_away']'''
-        # Defenders
-        # Drop penalties_saved, saves
-        # Add based on team: strength_defence_home, strength_defence_away
-        elif position == 'DEF':
-            gw_data = gw_data.drop(['penalties_saved', 'saves'], axis=1)
-            '''for index, row in gw_data.iterrows():
-                if row['was_home']:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_defence_home']
-                else:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_defence_away']'''
-        # Midfielders
-        # Drop penalties_saved, saves, clean_sheets
-        # Add based on team: strength_overall_home, strength_overall_away
-        elif position == 'MID':
-            gw_data = gw_data.drop(['penalties_saved', 'saves', 'clean_sheets'], axis=1)
-            '''for index, row in gw_data.iterrows():
-                if row['was_home']:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_overall_home']
-                else:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_overall_away']'''
-        # Forwards
-        # Drop penalties_saved, saves, clean_sheets, goals_conceded
-        # Add based on team: strength_attack_home, strength_attack_away
-        elif position == 'FWD':
-            gw_data = gw_data.drop(['penalties_saved', 'saves', 'clean_sheets', 'goals_conceded'], axis=1)
-            '''for index, row in gw_data.iterrows():
-                if row['was_home']:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_attack_home']
-                else:
-                    gw_data.at[index, 'team'] = self.team_list.loc[row['team']]['strength_attack_away']'''
-            
-        else:
-            print(f'Invalid position: {position}')
-            quit()
-
+        gw_data = gw_data.drop(['position', 'team'], axis=1)
         return gw_data
     
     def get_training_data(self, week_num):
@@ -88,24 +41,35 @@ class vastaav_data:
         mid_features = self.get_pos_data(week_num, 'MID')
         fwd_features = self.get_pos_data(week_num, 'FWD')
 
-        # Remove all players that have not played
-        gk_features = gk_features[gk_features['minutes'] > 0]
-        def_features = def_features[def_features['minutes'] > 0]
-        mid_features = mid_features[mid_features['minutes'] > 0]
-        fwd_features = fwd_features[fwd_features['minutes'] > 0]
-        
-        # Seperate labels from features
-        gk_labels = gk_features['total_points']
-        def_labels = def_features['total_points']
-        mid_labels = mid_features['total_points']
-        fwd_labels = fwd_features['total_points']
+        # Get the 'total_points' label from next gameweek
+        gk_labels = self.get_pos_data(week_num + 1, 'GK')
+        def_labels = self.get_pos_data(week_num + 1, 'DEF')
+        mid_labels = self.get_pos_data(week_num + 1, 'MID')
+        fwd_labels = self.get_pos_data(week_num + 1, 'FWD')
 
-        # Drop the name column
-        gk_features = gk_features.drop(['was_home', 'team', 'position', 'total_points'], axis=1)
-        def_features = def_features.drop(['was_home', 'team', 'position', 'total_points'], axis=1)
-        mid_features = mid_features.drop(['was_home', 'team', 'position', 'total_points'], axis=1)
-        fwd_features = fwd_features.drop(['was_home', 'team', 'position', 'total_points'], axis=1)
-        
+        # Keep only players that are in both the features and labels
+        gk_features = gk_features[gk_features.index.isin(gk_labels.index)]
+        def_features = def_features[def_features.index.isin(def_labels.index)]
+        mid_features = mid_features[mid_features.index.isin(mid_labels.index)]
+        fwd_features = fwd_features[fwd_features.index.isin(fwd_labels.index)]
+
+        gk_labels = gk_labels[gk_labels.index.isin(gk_features.index)]['total_points']
+        def_labels = def_labels[def_labels.index.isin(def_features.index)]['total_points']
+        mid_labels = mid_labels[mid_labels.index.isin(mid_features.index)]['total_points']
+        fwd_labels = fwd_labels[fwd_labels.index.isin(fwd_features.index)]['total_points']
+
+        # Drop the remaining columns that are not features
+        gk_features = gk_features.drop(['total_points'], axis=1)
+        def_features = def_features.drop(['total_points'], axis=1)
+        mid_features = mid_features.drop(['total_points'], axis=1)
+        fwd_features = fwd_features.drop(['total_points'], axis=1)
+
+        # Get the player names in a list from each pos
+        gk_names_list = list(gk_features.index)
+        def_names_list = list(def_features.index)
+        mid_names_list = list(mid_features.index)
+        fwd_names_list = list(fwd_features.index)
+
         # Get the feature names in a list from each pos
         gk_feature_list = list(gk_features.columns)
         def_feature_list = list(def_features.columns)
@@ -129,5 +93,36 @@ class vastaav_data:
         training_mid = (mid_features, mid_labels)
         training_fwd = (fwd_features, fwd_labels)
 
-        return [gk_feature_list, def_feature_list, mid_feature_list, fwd_feature_list], [training_gk, training_def, training_mid, training_fwd]
-    
+        return [gk_names_list, def_names_list, mid_names_list, fwd_names_list], [gk_feature_list, def_feature_list, mid_feature_list, fwd_feature_list], [training_gk, training_def, training_mid, training_fwd]
+
+    def get_test_data(self, week_num):
+        gk_features = self.get_pos_data(week_num, 'GK')
+        def_features = self.get_pos_data(week_num, 'DEF')
+        mid_features = self.get_pos_data(week_num, 'MID')
+        fwd_features = self.get_pos_data(week_num, 'FWD')
+
+        # Drop the remaining columns that are not features
+        gk_features = gk_features.drop(['total_points'], axis=1)
+        def_features = def_features.drop(['total_points'], axis=1)
+        mid_features = mid_features.drop(['total_points'], axis=1)
+        fwd_features = fwd_features.drop(['total_points'], axis=1)
+
+        # Get the player names in a list from each pos
+        gk_names_list = list(gk_features.index)
+        def_names_list = list(def_features.index)
+        mid_names_list = list(mid_features.index)
+        fwd_names_list = list(fwd_features.index)
+
+        # Get the feature names in a list from each pos
+        gk_feature_list = list(gk_features.columns)
+        def_feature_list = list(def_features.columns)
+        mid_feature_list = list(mid_features.columns)
+        fwd_feature_list = list(fwd_features.columns)
+
+        # Convert features and labels to numpy arrays
+        gk_features = gk_features.to_numpy()
+        def_features = def_features.to_numpy()
+        mid_features = mid_features.to_numpy()
+        fwd_features = fwd_features.to_numpy()
+
+        return [gk_names_list, def_names_list, mid_names_list, fwd_names_list], [gk_feature_list, def_feature_list, mid_feature_list, fwd_feature_list], [gk_features, def_features, mid_features, fwd_features]
