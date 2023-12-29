5b66dbac5369d9030cca7cea1c4652ab35b04e16
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-12-29 16:39:53 +0000

    Code supports testing within dataset

--- main.py
+++ main.py
@@ -5,36 +5,44 @@ Author: Benjamin Tindal
 
 import numpy as np
 import pandas as pd
-import requests, json
+import math
 from pprint import pprint
 import json
 from fplapi import fplapi_data
 from vastaav import vastaav_data
 from evaluate import fpl_evaluate
-from sklearn import linear_model
-from sklearn import ensemble  
+from sklearn import linear_model, ensemble
+import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor 
 import matplotlib.pyplot as plt
 
 target_season = '2023-24'
-target_gameweek = 9
-last_gameweek = 17
-modelType = 'randomforest' # linear or randomforest
+target_gameweek = 15
+last_gameweek = 18
+repeat = 5
+modelType = 'linear' # linear, randomforest, xgboost, gradientboost
 display_weights = False
+plot_predictions = False
+
 fplapi = fplapi_data(target_season)
 vastaav = vastaav_data('../Fantasy-Premier-League/data', target_season)
 eval = fpl_evaluate()
 
 def main():
+    count = 0
+    total_e = 0
+    total_ase = 0
+    total_aa = 0
+    
     # Predict points for GWi
-    for i in range(target_gameweek, target_gameweek + 5):
+    for i in range(target_gameweek, target_gameweek + repeat):
         # Retrain model each time
         # Lets sum up the last 3 gameweeks to get a more accurate representation of player performance
-        if i >= last_gameweek:
+        if i > last_gameweek:
             print(f'GW{i}: n/a')
-            __, feature_names, week_data = vastaav.get_training_data(last_gameweek - 1)
+            feature_names, training_data, test_data = vastaav.get_training_data_all(last_gameweek - 6, last_gameweek - 1)
         else:
-            __, feature_names, week_data = vastaav.get_training_data(i - 1)
+            feature_names, training_data, test_data = vastaav.get_training_data_all(i - 6, i - 1)
 
         # Lets train a model
         if modelType == 'linear':
@@ -42,60 +50,72 @@ def main():
             def_model = linear_model.LinearRegression()
             mid_model = linear_model.LinearRegression()
             fwd_model = linear_model.LinearRegression()
+            
         elif modelType == 'randomforest':
             gk_model = RandomForestRegressor(oob_score = True, n_estimators = 1000, max_features = 5)
             def_model = RandomForestRegressor(oob_score = True, n_estimators = 1000, max_features = 5)
             mid_model = RandomForestRegressor(oob_score = True, n_estimators = 1000, max_features = 5)
             fwd_model = RandomForestRegressor(oob_score = True, n_estimators = 1000, max_features = 5)
 
-        gk_model.fit(week_data[0][0], week_data[0][1])
-        def_model.fit(week_data[1][0], week_data[1][1])
-        mid_model.fit(week_data[2][0], week_data[2][1])
-        fwd_model.fit(week_data[3][0], week_data[3][1])
+        elif modelType == 'xgboost':
+            gk_model = xgb.XGBRegressor()
+            def_model = xgb.XGBRegressor()
+            mid_model = xgb.XGBRegressor()
+            fwd_model = xgb.XGBRegressor()
 
-        if modelType == 'linear' and display_weights:
-            # Plot the weights to each feature
-            weights = np.concatenate((gk_model.coef_, def_model.coef_, mid_model.coef_, fwd_model.coef_))
-            feature_names = np.concatenate((feature_names[0], feature_names[1], feature_names[2], feature_names[3]))
+        elif modelType == 'gradientboost':
+            loss_function = 'squared_error'
+            n_est = 1000 # keep at 1000 whilst in development for speed
+            l_rate = 0.2
+            gk_model = ensemble.GradientBoostingRegressor(max_features=17, n_estimators=n_est, learning_rate=l_rate, loss=loss_function)
+            def_model = ensemble.GradientBoostingRegressor(max_features=17,n_estimators=n_est, learning_rate=l_rate, loss=loss_function)
+            mid_model = ensemble.GradientBoostingRegressor(max_features=17,n_estimators=n_est, learning_rate=l_rate, loss=loss_function)
+            fwd_model = ensemble.GradientBoostingRegressor(max_features=17,n_estimators=n_est, learning_rate=l_rate, loss=loss_function)
 
-            plt.title(f'Feature weights for GW{i}')
+        gk_model.fit(training_data[0][0], training_data[0][1])
+        def_model.fit(training_data[1][0], training_data[1][1])
+        mid_model.fit(training_data[2][0], training_data[2][1])
+        fwd_model.fit(training_data[3][0], training_data[3][1])
+
+        if display_weights:
+            ''' NOT WORKING PROPERLY
+            - Feature names are not in the same order as the weights
+            - Need to find a way to get the feature names in the same order as the weights'''
+
+            # Plot feature weights
+            # Get the feature weights from each model
+            gk_weights = gk_model.coef_
+            def_weights = def_model.coef_
+            mid_weights = mid_model.coef_
+            fwd_weights = fwd_model.coef_
+
+            # Plot the weights
+            plt.title(f'GW{i} feature weights for MID')
             plt.xlabel('Feature')
             plt.ylabel('Weight')
-            plt.bar(feature_names, weights)
+            plt.bar(feature_names, mid_weights)
             plt.xticks(rotation=90)
+            plt.tight_layout()
             plt.show()
 
-        # target_gameweek has not happened yet, so we predict it using past data 
-        # Get test data by summing up 3 previous gameweeks
-        # week_data = GWi-3 + GWi-2 + GWi-1
-        if i >= last_gameweek:
-            # Get features from most up to date data
-            player_names, feature_names, features = vastaav.get_test_data(last_gameweek)
-        else: 
-            player_names, feature_names, test_data = vastaav.get_training_data(i)
-        
-            # Get the features and labels from the test data
-            features = [test_data[0][0], test_data[1][0], test_data[2][0], test_data[3][0]]
-            labels = [test_data[0][1], test_data[1][1], test_data[2][1], test_data[3][1]] 
+        gk_predictions = np.round(gk_model.predict(test_data[0][0]), 3)
+        def_predictions = np.round(def_model.predict(test_data[1][0]), 3)
+        mid_predictions = np.round(mid_model.predict(test_data[2][0]), 3)
+        fwd_predictions = np.round(fwd_model.predict(test_data[3][0]), 3)
 
-        gk_predictions = np.round(gk_model.predict(features[0]), 3)
-        def_predictions = np.round(def_model.predict(features[1]), 3)
-        mid_predictions = np.round(mid_model.predict(features[2]), 3)
-        fwd_predictions = np.round(fwd_model.predict(features[3]), 3)
-
-        if i < last_gameweek:
-            gk_error, gk_square_error, gk_accuracy = eval.score_model(gk_predictions, labels[0])
-            def_error, def_square_error, def_accuracy = eval.score_model(def_predictions, labels[1])
-            mid_error, mid_square_error, mid_accuracy = eval.score_model(mid_predictions, labels[2])
-            fwd_error, fwd_square_error, fwd_accuracy = eval.score_model(fwd_predictions, labels[3])
+        if i <= last_gameweek:
+            gk_error, gk_square_error, gk_accuracy = eval.score_model(gk_predictions, test_data[0][1])
+            def_error, def_square_error, def_accuracy = eval.score_model(def_predictions, test_data[1][1])
+            mid_error, mid_square_error, mid_accuracy = eval.score_model(mid_predictions, test_data[2][1])
+            fwd_error, fwd_square_error, fwd_accuracy = eval.score_model(fwd_predictions, test_data[3][1])
 
             # Average the errors
             error = (gk_error + def_error + mid_error + fwd_error) / 4
             ase = (gk_square_error + def_square_error + mid_square_error + fwd_square_error) / 4
             aa = (gk_accuracy + def_accuracy + mid_accuracy + fwd_accuracy) / 4
-            print(f'GW{i}: AE: {error:.2f}, ASE: {ase:.2f}, ACC: {aa*100:.2f}%')
+            print(f'GW{i}: AE: {error:.3f}, ASE: {math.sqrt(ase):.3f}, ACC: {aa*100:.2f}%')
 
-        if i < last_gameweek:
+        '''if i <= last_gameweek:
             # Output predictions to csv, use player_names as first column, then predictions, then actual points
             # This will allow us to compare predictions to actual points
             csv_gk_predictions = np.column_stack((player_names[0], gk_predictions, labels[0]))
@@ -118,27 +138,34 @@ def main():
         csv_predictions.set_index('Name', inplace=True)
 
         csv_predictions.to_csv(f'predictions/predictions_{modelType}_gw{i}.csv')
-        
-        '''# Plot predictions vs actual points for GWi
-        # Colour code by position
-        plt.title(f'GW{i} predictions vs actual points')
-        plt.xlabel('Predicted points')
-        plt.ylabel('Actual points')
-        print(len(gk_predictions))
-        print(len(labels[0]))
-        plt.scatter(gk_predictions, labels[0], color='red')
-        plt.scatter(def_predictions, labels[1], color='blue')
-        plt.scatter(mid_predictions, labels[2], color='green')
-        plt.scatter(fwd_predictions, labels[3], color='orange')
-        plt.legend(['GK', 'DEF', 'MID', 'FWD'])
-        # Plot y=x line to show perfect prediction
-        plt.plot([0, 20], [0, 20], color='black')
-        plt.show()'''
+        '''
+        if plot_predictions:
+            # Plot predictions vs actual points for GWi
+            # Colour code by position
+            plt.title(f'GW{i} predictions vs actual points')
+            plt.xlabel('Predicted points')
+            plt.ylabel('Actual points')
+            plt.scatter(gk_predictions, test_data[0][1], color='red')
+            plt.scatter(def_predictions, test_data[1][1], color='blue')
+            plt.scatter(mid_predictions, test_data[2][1], color='green')
+            plt.scatter(fwd_predictions, test_data[3][1], color='orange')
 
+            plt.legend(['GK', 'DEF', 'MID', 'FWD'])
+            # Plot y=x line to show perfect prediction
+            plt.plot([0, 20], [0, 20], color='black')
+            plt.show()
 
+        if i <= last_gameweek:
+            count += 1
+            total_e += error
+            total_ase += ase
+            total_aa += aa
+    
+    total_e /= count
+    total_ase /= count
+    total_aa /= count
 
-
-
+    print(f'Count: {count}, Average AE: {total_e:.2f}, Average ASE: {math.sqrt(total_ase):.2f}, Average ACC: {total_aa*100:.2f}%')
 
 if __name__ == "__main__":
     main()
--- vastaav.py
+++ vastaav.py
@@ -4,6 +4,9 @@ import requests
 import json
 from pprint import pprint
 import json
+import sklearn
+from sklearn import linear_model, ensemble, model_selection
+from sklearn.model_selection import train_test_split
 
 class vastaav_data:
     def __init__(self, data_location, season):
@@ -41,22 +44,10 @@ class vastaav_data:
         mid_features = self.get_pos_data(week_num, 'MID')
         fwd_features = self.get_pos_data(week_num, 'FWD')
 
-        # Get the 'total_points' label from next gameweek
-        gk_labels = self.get_pos_data(week_num + 1, 'GK')
-        def_labels = self.get_pos_data(week_num + 1, 'DEF')
-        mid_labels = self.get_pos_data(week_num + 1, 'MID')
-        fwd_labels = self.get_pos_data(week_num + 1, 'FWD')
-
-        # Keep only players that are in both the features and labels
-        gk_features = gk_features[gk_features.index.isin(gk_labels.index)]
-        def_features = def_features[def_features.index.isin(def_labels.index)]
-        mid_features = mid_features[mid_features.index.isin(mid_labels.index)]
-        fwd_features = fwd_features[fwd_features.index.isin(fwd_labels.index)]
-
-        gk_labels = gk_labels[gk_labels.index.isin(gk_features.index)]['total_points']
-        def_labels = def_labels[def_labels.index.isin(def_features.index)]['total_points']
-        mid_labels = mid_labels[mid_labels.index.isin(mid_features.index)]['total_points']
-        fwd_labels = fwd_labels[fwd_labels.index.isin(fwd_features.index)]['total_points']
+        gk_labels = gk_features['total_points']
+        def_labels = def_features['total_points']
+        mid_labels = mid_features['total_points']
+        fwd_labels = fwd_features['total_points']
 
         # Drop the remaining columns that are not features
         gk_features = gk_features.drop(['total_points'], axis=1)
@@ -64,17 +55,7 @@ class vastaav_data:
         mid_features = mid_features.drop(['total_points'], axis=1)
         fwd_features = fwd_features.drop(['total_points'], axis=1)
 
-        # Get the player names in a list from each pos
-        gk_names_list = list(gk_features.index)
-        def_names_list = list(def_features.index)
-        mid_names_list = list(mid_features.index)
-        fwd_names_list = list(fwd_features.index)
-
-        # Get the feature names in a list from each pos
-        gk_feature_list = list(gk_features.columns)
-        def_feature_list = list(def_features.columns)
-        mid_feature_list = list(mid_features.columns)
-        fwd_feature_list = list(fwd_features.columns)
+        feature_names = list(gk_features.columns)
 
         # Convert features and labels to numpy arrays
         gk_features = gk_features.to_numpy()
@@ -93,7 +74,41 @@ class vastaav_data:
         training_mid = (mid_features, mid_labels)
         training_fwd = (fwd_features, fwd_labels)
 
-        return [gk_names_list, def_names_list, mid_names_list, fwd_names_list], [gk_feature_list, def_feature_list, mid_feature_list, fwd_feature_list], [training_gk, training_def, training_mid, training_fwd]
+        return feature_names, training_gk, training_def, training_mid, training_fwd
+
+    def get_training_data_all(self, from_gw, to_gw):
+        feature_names = []
+        for i in range(from_gw, to_gw):
+            if i == from_gw:
+                feature_names, training_gk, training_def, training_mid, training_fwd = self.get_training_data(i)
+            else:
+                feature_names, training_gk_new, training_def_new, training_mid_new, training_fwd_new = self.get_training_data(i)
+                training_gk = (np.concatenate((training_gk[0], training_gk_new[0])), np.concatenate((training_gk[1], training_gk_new[1])))
+                training_def = (np.concatenate((training_def[0], training_def_new[0])), np.concatenate((training_def[1], training_def_new[1])))
+                training_mid = (np.concatenate((training_mid[0], training_mid_new[0])), np.concatenate((training_mid[1], training_mid_new[1])))
+                training_fwd = (np.concatenate((training_fwd[0], training_fwd_new[0])), np.concatenate((training_fwd[1], training_fwd_new[1])))
+
+            feature_names = list(set(feature_names) & set(feature_names))
+
+        gk_features_train, gk_features_test, gk_labels_train, gk_labels_test = train_test_split(training_gk[0], training_gk[1], test_size=0.2, random_state=42)
+        def_features_train, def_features_test, def_labels_train, def_labels_test = train_test_split(training_def[0], training_def[1], test_size=0.2, random_state=42)
+        mid_features_train, mid_features_test, mid_labels_train, mid_labels_test = train_test_split(training_mid[0], training_mid[1], test_size=0.2, random_state=42)
+        fwd_features_train, fwd_features_test, fwd_labels_train, fwd_labels_test = train_test_split(training_fwd[0], training_fwd[1], test_size=0.2, random_state=42)
+
+        training_gk = (gk_features_train, gk_labels_train)
+        training_def = (def_features_train, def_labels_train)
+        training_mid = (mid_features_train, mid_labels_train)
+        training_fwd = (fwd_features_train, fwd_labels_train)
+
+        test_gk = (gk_features_test, gk_labels_test)
+        test_def = (def_features_test, def_labels_test)
+        test_mid = (mid_features_test, mid_labels_test)
+        test_fwd = (fwd_features_test, fwd_labels_test)
+
+        training_data = [training_gk, training_def, training_mid, training_fwd]
+        test_data = [test_gk, test_def, test_mid, test_fwd]
+
+        return feature_names, training_data, test_data
 
     def get_test_data(self, week_num):
         gk_features = self.get_pos_data(week_num, 'GK')
@@ -114,10 +129,10 @@ class vastaav_data:
         fwd_names_list = list(fwd_features.index)
 
         # Get the feature names in a list from each pos
-        gk_feature_list = list(gk_features.columns)
-        def_feature_list = list(def_features.columns)
-        mid_feature_list = list(mid_features.columns)
-        fwd_feature_list = list(fwd_features.columns)
+        gk_feature_names = list(gk_features.columns)
+        def_feature_names = list(def_features.columns)
+        mid_feature_names = list(mid_features.columns)
+        fwd_feature_names = list(fwd_features.columns)
 
         # Convert features and labels to numpy arrays
         gk_features = gk_features.to_numpy()
@@ -125,4 +140,4 @@ class vastaav_data:
         mid_features = mid_features.to_numpy()
         fwd_features = fwd_features.to_numpy()
 
-        return [gk_names_list, def_names_list, mid_names_list, fwd_names_list], [gk_feature_list, def_feature_list, mid_feature_list, fwd_feature_list], [gk_features, def_features, mid_features, fwd_features]
+        return [gk_names_list, def_names_list, mid_names_list, fwd_names_list], [gk_feature_names, def_feature_names, mid_feature_names, fwd_feature_names], [gk_features, def_features, mid_features, fwd_features]
